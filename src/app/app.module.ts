737c51dfe9b7d0ca0cd0ffa35865ed38232d5b3a
김예은 <kye76362766@gmail.com>
2022-09-26 17:25:05 +0900

    layout finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './pages/layout/layout.component';
-import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
