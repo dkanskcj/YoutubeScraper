f8b5a312e267d450529e078deb6f05c9ad378fe9
dkanskcj <dkanskcj@likelion.org>
2022-09-27 17:38:01 +0900

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { CommentpageModule } from './pages/commentpage/commentpage.module';
+import { WholepageModule } from './wholepage/wholepage.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -19,7 +20,8 @@ import { CommentpageModule } from './pages/commentpage/commentpage.module';
     ComponentsModule,
     CommentpageModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    WholepageModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,8 +5,9 @@ import { ButtonsComponent } from './buttons/buttons.component';
 import { CategoryComponent } from './category/category.component';
 import { VideosComponent } from './videos/videos.component';
 import { CommentComponent } from './comment/comment.component';
+import { ButtonComponent } from './button/button.component';
 
-const components = [ InputsComponent, ButtonsComponent, CategoryComponent, VideosComponent, CommentComponent ]
+const components = [ InputsComponent, ButtonsComponent, CategoryComponent, VideosComponent, CommentComponent, ButtonComponent ]
 
 @NgModule({
   declarations: components,
